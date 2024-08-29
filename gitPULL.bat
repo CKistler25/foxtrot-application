603d9f407b033228871b397798f0788521f77488
CKistler25 <carter.kistler25@gmail.com>
2024-08-29 09:20:45 -0400

    removed extraneous files from repo

--- gitPULL.bat
+++
@@ -1,3 +0,0 @@
-git add .
-git commit -m "pulling lecture"
-git pull upstream main
--- gitPUSH.bat
+++
@@ -1,5 +0,0 @@
-set/p commit_message="Enter message: "
-
-git add .
-git commit -m "%commit_message%"
-git push origin main
